e63cb5aa35b99dae4c53cc6516f4367fb4bb9641
VILFREDSAGEN <olegsbmk@gmail.com>
2024-12-10 15:43:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+"1st programm"
+print(9**0.5*5)
+"2nd programm"
+#True False
+print(9.99>9.98)and(100!=1000.1)
+"3rd program"
+print(2*2+2)
+print(2*(2+2))
+#True False
+print((2*2+2)==(2*(2+2)))
+"4th program"
+print("123.456")
+print(float(123.456))
+num=float
+print(123.456*10)
+print(int(1234.56))
+
+
+
